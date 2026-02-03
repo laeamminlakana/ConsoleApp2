70db2fbbcac79b6971a19e13155eddb3ec2234d3
laeamminlakana <vecumatt@gmail.com>
2026-02-03 15:56:29 +0200

    Add files via upload
    Added serialization and early iteration of laser task

--- ConsoleApp2/Detection1.cs
+++ ConsoleApp2/Detection1.cs
@@ -4,8 +4,11 @@ using Emgu.CV.Features2D;
 using Emgu.CV.Linemod;
 using Emgu.CV.Structure;
 using Emgu.CV.Util;
+using System.IO;
+using System.Text.Json;
+using System.Drawing;
 
-namespace ourNamespace;
+namespace ConsoleApp2;
 public class Detection1
 {
     static void Main(string[] args)
@@ -17,7 +20,7 @@ public class Detection1
 
         if (!File.Exists(cfgPath) || !File.Exists(weightsPath) || !File.Exists(namesPath))
             throw new FileNotFoundException($"Missing files: {cfgPath}, {weightsPath}, {namesPath}");
-        
+
         var net = DnnInvoke.ReadNet(cfgPath, weightsPath);
         var classLabels = File.ReadAllLines(namesPath);
 
@@ -78,7 +81,7 @@ public class Detection1
                         var x = (int)(centerX - (boxWidth / 2));
                         var y = (int)(centerY - (boxHeight / 2));
 
-                        boxes.Push(new System.Drawing.Rectangle[] { new System.Drawing.Rectangle(x, y, boxWidth, boxHeight ) });
+                        boxes.Push(new System.Drawing.Rectangle[] { new System.Drawing.Rectangle(x, y, boxWidth, boxHeight) });
                         indices.Push(new int[] { classId });
                         scores.Push(new float[] { confidence });
                         aimTarget.Push(new System.Drawing.Point[] { new System.Drawing.Point(centerX, centerY) });
@@ -98,8 +101,8 @@ public class Detection1
                 var box = boxes[index];
                 var bob = aimTarget[i];
                 CvInvoke.Rectangle(frameOut, box, new MCvScalar(0, 255, 0), 1);
-                CvInvoke.DrawMarker(frameOut, bob, new MCvScalar(255, 255, 255), Emgu.CV.CvEnum.MarkerTypes.Cross,1,1);
-                
+                CvInvoke.DrawMarker(frameOut, bob, new MCvScalar(255, 255, 255), Emgu.CV.CvEnum.MarkerTypes.Cross, 1, 1);
+
                 CvInvoke.PutText(frameOut, classLabels[indices[index]], new System.Drawing.Point(box.X, box.Y - 20),
                 Emgu.CV.CvEnum.FontFace.HersheyPlain, 1.0, new MCvScalar(0, 0, 255), 1);
 
@@ -108,9 +111,23 @@ public class Detection1
             CvInvoke.Resize(frameOut, frameOut, new System.Drawing.Size(0, 0), 4, 4);
             CvInvoke.Imshow("output", frameOut);
 
+            // Convert aimTarget to array of points and process only if there is at least one point.
+            System.Drawing.Point[] points = aimTarget.ToArray();
+            if (points.Length > 0)
+            {
+                var aimpoint = points[0];
+
+                if (aimpoint.X != 0 && aimpoint.Y != 0)
+                {
+                    var jsonstring1 = System.Text.Json.JsonSerializer.Serialize(aimpoint);
+                    File.WriteAllText("aimTarget.json", jsonstring1);
+                    Task.Run(() => Laser.Main_Laser(args));
+                }
+
+
+            }
             if (CvInvoke.WaitKey(1) == 27)
                 break;
-
         }
     }
 }
---
+++ ConsoleApp2/Laser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Drawing;
+namespace ConsoleApp2;
+
+
+
+public static class Laser
+{
+	
+    public static void Main_Laser(string[] args)
+	{
+        const string path = "aimTarget.json";
+        string jsonstring;
+            jsonstring = File.ReadAllText(path);
+
+            Point kohde = JsonSerializer.Deserialize<Point>(jsonstring);
+            Console.WriteLine("Laser targeting point: X" + kohde.X + " and " + kohde.Y);
+
+    }
+}
